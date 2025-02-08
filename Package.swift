d23c9702f02d336bfcb4593b6c281994efcdaa9c
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-02-09 05:13:25 +0900

    format

--- Package.swift
+++ Package.swift
@@ -1,21 +1,19 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "EDF",
+    name: "swift-edf",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "EDF",
-            targets: ["EDF"]),
+            targets: ["EDF"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "EDF"),
+            name: "EDF"
+        ),
         .testTarget(
             name: "EDFTests",
             dependencies: ["EDF"]
